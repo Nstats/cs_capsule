77e64a47f7b76222ead2e31e9fa902dc028d9b33
Nstats <17801238715@163.com>
2019-05-20 15:40:32 +0800

    5.20

--- examples/shell/run_squad_capsule_base.sh
+++ examples/shell/run_squad_capsule_base.sh
@@ -7,7 +7,7 @@ python ../run_squad_capsule.py \
 	  --train_file ../../data/train-v2.0.json \
           --predict_file ../../data/dev-v2.0.json\
           --train_batch_size 24 \
-          --learning_rate 0.05 \
+          --learning_rate 5e-5 \
 	  --num_train_epochs 3 \
           --max_seq_length 300 \
           --doc_stride 128 \
@@ -17,7 +17,6 @@ python ../run_squad_capsule.py \
           --log_every_step 100 \
           --eveluate_every_step 3000\
 	  --save_model_every_step 6000\
-	  --learning_rate 0.05
 
 # nohup sh run_squad_capsule_base.sh > ../log/run_squad_capsule_base.log_cs 2>&1 &
 # tail -f ../log/run_squad_capsule_base.log_cs
